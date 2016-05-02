4322e6bc2c54ed62ce7382bca40fe43e7f64c4e5
Ryutaro Kawaguchi <ryutaro.kawaguchi@mfro.net>
2016-05-02 16:09:44 +0900

    first commit

---
+++ httpserver/apache/attributes/default.rb
@@ -0,0 +1,3 @@
+
+default['apache']['admin'] = 'root@test-domain.com'
+default['apache']['servername'] = 'www.test-domain.com'
---
+++ httpserver/apache/recipes/default.rb
@@ -0,0 +1,47 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# Download Remi repository
+remote_file "/tmp/remi-release-6.rpm" do
+    source "http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
+    action :create
+end
+
+# Install Remi repository
+rpm_package "remi-release-6" do
+    source "/tmp/remi-release-5.rpm"
+    action :install
+end
+
+# Install Apache httpd and PHP
+%[
+    httpd
+    php
+    php-mbstring
+    php-pear
+].each do |pkg|
+    package "#{pkg}" do
+        action :install
+	options '--enablerepo=remi-php56'
+    end
+end
+
+
+# make httpd.conf
+template "httpd.conf" do
+    source "httpd.conf.erb"
+    path "/etc/httpd/conf/httpd.conf"
+    group "root"
+    owner "root"
+    mode "0644"
+    variables(
+        :admin=>node['apache']['admin'],
+	:servername=>node['apache']['servername']
+    )
+end
